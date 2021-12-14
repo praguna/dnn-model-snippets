155e10a5dab924789263c06a03a7af01eaeab425
praguna <praguna.manvi@gmail.com>
2021-12-14 20:39:57 +0530

    corrected enhanced rcnn

--- tested_on_quora_dataset/enchanced_rcnn.py
+++ tested_on_quora_dataset/enchanced_rcnn.py
@@ -8,6 +8,8 @@ from keras.layers import Bidirectional
 from keras.layers import GRU
 from keras.layers import Permute
 
+# have not added gating fusion
+
 def cnn_1(size = 60):
   input = Input(shape = (None, size))
   conv1 = tf.keras.layers.Conv1D(64, 1, activation = 'relu')(input)
@@ -89,9 +91,15 @@ def enhancedRCNN(num_words = 200000, EMBEDDING_DIM = 300, MAX_SEQUENCE_LENGTH = 
 
     #attention
     c1, c2 = soft_align(size)([rnn_1, rnn_2])
+
+    # interactive modelling
+    va = tf.subtract(concatenate([rnn_1, c1, rnn_1], axis = 0),  concatenate([rnn_1, tf.multiply(rnn_1, c1)], axis = 0))
+    vb = tf.subtract(concatenate([rnn_2, c2, rnn_2], axis = 0),  concatenate([rnn_2, tf.multiply(rnn_2, c2)], axis = 0))
+
     M = mean_max(size)
-    sentence1_att_mean, sentence1_att_max =  M(c1)
-    sentence2_att_mean, sentence2_att_max = M(c2)
+    sentence1_att_mean, sentence1_att_max =  M(va)
+    sentence2_att_mean, sentence2_att_max = M(vb)
+
 
     #merge
     sentence1 = concatenate([sentence1_att_mean, sentence1_cnn, sentence1_att_max])
@@ -99,11 +107,11 @@ def enhancedRCNN(num_words = 200000, EMBEDDING_DIM = 300, MAX_SEQUENCE_LENGTH = 
 
     #subtract
     subtract = Lambda(lambda x : x[0] - x[1])([sentence1, sentence2])
-    d1 = Dense(512, activation = 'relu')(subtract)
+    d1 = Dense(256, activation = 'relu')(subtract)
 
     #multiply
     subtract = Lambda(lambda x : x[0] * x[1])([sentence1, sentence2])
-    d2 = Dense(512, activation = 'relu')(subtract)
+    d2 = Dense(256, activation = 'relu')(subtract)
 
 
     #final layer
